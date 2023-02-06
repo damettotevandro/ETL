8e2d6ff5de6f82a661a39be148e422e069fc7bfb
damettotevandro <80593672+damettotevandro@users.noreply.github.com>
2023-02-06 17:41:31 -0300

    Create __init__.py

---
+++ ETL/utils/database_connection/__init__.py
@@ -0,0 +1,5 @@
+from .oracle_db import OracleDataBase
+from .postgres_db import PostgresDataBase
+from .firebird_db import FirebirdDataBase
+from .mysql_db import MySqlDataBase
+from .sqlserver_db import SqlServerDataBase
